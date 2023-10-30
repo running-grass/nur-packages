0067331ebfb43c67b44f403d9d182193975143a2
running-grass <467195537@qq.com>
2023-10-30 16:48:35 +0800

    add listen1

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  listen1 = pkgs.callPackage ./pkgs/listen1 { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
